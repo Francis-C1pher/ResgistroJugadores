2229fb0d11661c91109281614cb0f20f9ce35179
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-09-06 18:48:00 -0400

    Creando La AppDatbase para el proyecto

--- app/src/main/java/edu/ucne/resgistrojugadores/Data/DataBase/AppDatabase.kt
+++ app/src/main/java/edu/ucne/resgistrojugadores/Data/DataBase/AppDatabase.kt
@@ -1,4 +1,36 @@
-package edu.ucne.resgistrojugadores.Data.DataBase
+package edu.ucne.RegistroJugadores.Data.DataBase
 
-class AppDatabase {
+import edu.ucne.RegistroJugadores.Data.Dao.JugadorDao
+import edu.ucne.RegistroJugadores.Data.Entities.JugadorEntity
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import android.content.Context
+
+
+
+@Database(
+    entities = [JugadorEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun jugadorDao(): JugadorDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "jugadores_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
