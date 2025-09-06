a116b73b2a1b711da4191cce679c954bcbd8c899
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-09-06 19:11:06 -0400

    Programando el archivo MainActivity.kt

--- app/src/main/java/edu/ucne/resgistrojugadores/MainActivity.kt
+++ app/src/main/java/edu/ucne/resgistrojugadores/MainActivity.kt
@@ -1,47 +1,27 @@
-package edu.ucne.resgistrojugadores
+package edu.ucne.RegistroJugadores
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import edu.ucne.resgistrojugadores.ui.theme.ResgistroJugadoresTheme
+import edu.ucne.RegistroJugadores.ui.screens.RegistroJugadorScreen
+import edu.ucne.RegistroJugadores.ui.theme.RegistroJugadoresTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ResgistroJugadoresTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            RegistroJugadoresTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RegistroJugadorScreen()  
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ResgistroJugadoresTheme {
-        Greeting("Android")
     }
 }
