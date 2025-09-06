eaf63938501f44ebaeba1f7322d978182d07ddae
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-09-06 18:47:11 -0400

    Programando el archivoJugadorDao

--- app/src/main/java/edu/ucne/resgistrojugadores/Data/Dao/JugadorDao.kt
+++ app/src/main/java/edu/ucne/resgistrojugadores/Data/Dao/JugadorDao.kt
@@ -1,4 +1,29 @@
-package edu.ucne.resgistrojugadores.Data.Dao
+package edu.ucne.RegistroJugadores.Data.Dao
 
-class JugadorDao {
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import edu.ucne.RegistroJugadores.Data.Entities.JugadorEntity
+@Dao
+interface JugadorDao {
+    @Insert
+    suspend fun insert(jugador: JugadorEntity): Long
+
+    @Update
+    suspend fun update(jugador: JugadorEntity)
+
+    @Delete
+    suspend fun delete(jugador: JugadorEntity)
+
+    @Query("SELECT * FROM Jugadores")
+    fun getAll(): Flow<List<JugadorEntity>>
+
+    @Query("SELECT * FROM Jugadores WHERE jugadorId = :id")
+    suspend fun getById(id: Int): JugadorEntity?
+
+    @Query("SELECT * FROM Jugadores WHERE nombres = :nombre")
+    suspend fun getByNombre(nombre: String): JugadorEntity?
+
+    @Query("SELECT COUNT(*) FROM Jugadores WHERE nombres = :nombre")
+    suspend fun existeNombre(nombre: String): Int
 }
