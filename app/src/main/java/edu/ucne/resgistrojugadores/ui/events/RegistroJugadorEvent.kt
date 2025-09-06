bcab14a603839a86703ffdfb93eec5ae3efbadf1
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-09-06 19:08:41 -0400

    programando el archivo RegistroJugadorEvent.kt y sus imports

--- app/src/main/java/edu/ucne/resgistrojugadores/ui/events/RegistroJugadorEvent.kt
+++ app/src/main/java/edu/ucne/resgistrojugadores/ui/events/RegistroJugadorEvent.kt
@@ -1,4 +1,11 @@
-package edu.ucne.resgistrojugadores.ui.events
+package edu.ucne.RegistroJugadores.ui.events
 
-class RegistroJugadorEvent {
+sealed class RegistroJugadorEvent {
+    data class NombresChanged(val nombres: String) : RegistroJugadorEvent()
+    data class PartidasChanged(val partidas: String) : RegistroJugadorEvent()
+    object SaveJugador : RegistroJugadorEvent()
+    object ClearForm : RegistroJugadorEvent()
+    data class DeleteJugador(val jugadorId: Int) : RegistroJugadorEvent()
+    data class SelectJugador(val jugadorId: Int) : RegistroJugadorEvent()
+
 }
