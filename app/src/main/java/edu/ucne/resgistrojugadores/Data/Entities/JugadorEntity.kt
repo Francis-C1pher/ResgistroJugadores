9a2ee70adbdd50dc4251b92504f0da4085c2bf54
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-09-06 18:52:06 -0400

    creando la entidad JugadorEntity.kt

--- app/src/main/java/edu/ucne/resgistrojugadores/Data/Entities/JugadorEntity.kt
+++ app/src/main/java/edu/ucne/resgistrojugadores/Data/Entities/JugadorEntity.kt
@@ -1,4 +1,12 @@
-package edu.ucne.resgistrojugadores.Data.Entities
+package edu.ucne.RegistroJugadores.Data.Entities
 
-class JugadorEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Jugadores")
+data class JugadorEntity(
+    @PrimaryKey(autoGenerate = true)
+    val jugadorId: Int = 0,
+    val nombres: String,
+    val partidas: Int
+)
