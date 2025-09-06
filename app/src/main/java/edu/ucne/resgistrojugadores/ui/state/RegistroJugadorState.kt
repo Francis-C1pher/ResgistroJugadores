e50f760fac115877d215cb4dfc2c0f2471bf169a
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-09-06 19:09:44 -0400

    Programando el archivo RegistroJugadorState.kt

--- app/src/main/java/edu/ucne/resgistrojugadores/ui/state/RegistroJugadorState.kt
+++ app/src/main/java/edu/ucne/resgistrojugadores/ui/state/RegistroJugadorState.kt
@@ -1,4 +1,16 @@
-package edu.ucne.resgistrojugadores.ui.state
+package edu.ucne.RegistroJugadores.ui.state
 
-class RegistroJugadorState {
-}
+
+import edu.ucne.RegistroJugadores.Domain.model.Jugador
+
+data class RegistroJugadorState(
+    val jugadorId: Int = 0,
+    val nombres: String = "",
+    val partidas: String = "",
+    val nombresError: String? = null,
+    val partidasError: String? = null,
+    val isLoading: Boolean = false,
+    val successMessage: String? = null,
+    val errorMessage: String? = null,
+    val jugadores: List<Jugador> = emptyList()
+)
