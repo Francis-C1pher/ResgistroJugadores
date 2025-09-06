9c0a428c18362dd97f01ee634fbe95f2307cd49b
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-09-06 18:55:09 -0400

    codificando el archivo Jugador Repository

--- app/src/main/java/edu/ucne/resgistrojugadores/Domain/repository/JugadorRepository.kt
+++ app/src/main/java/edu/ucne/resgistrojugadores/Domain/repository/JugadorRepository.kt
@@ -1,4 +1,14 @@
-package edu.ucne.resgistrojugadores.Domain.repository
+package edu.ucne.RegistroJugadores.Domain.repository
 
-class JugadorRepository {
+
+import edu.ucne.RegistroJugadores.Domain.model.Jugador
+import kotlinx.coroutines.flow.Flow
+
+interface JugadorRepository {
+    suspend fun insertJugador(jugador: Jugador): Long
+    suspend fun updateJugador(jugador: Jugador)
+    suspend fun deleteJugador(jugador: Jugador)
+    fun getAllJugadores(): Flow<List<Jugador>>
+    suspend fun getJugadorById(id: Int): Jugador?
+    suspend fun existeNombre(nombre: String): Boolean
 }
