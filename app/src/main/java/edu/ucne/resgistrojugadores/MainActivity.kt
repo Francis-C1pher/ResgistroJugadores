7f37e68e118ee04008f5851f2647f8ff21cd27f6
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-09-22 16:36:33 -0400

    Modificando el MainActivity.kt Para que muestre el AppNavigation.

--- app/src/main/java/edu/ucne/resgistrojugadores/MainActivity.kt
+++ app/src/main/java/edu/ucne/resgistrojugadores/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import edu.ucne.RegistroJugadores.ui.screens.RegistroJugadorScreen
+import edu.ucne.RegistroJugadores.ui.navigation.AppNavigation
 import edu.ucne.RegistroJugadores.ui.theme.RegistroJugadoresTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    RegistroJugadorScreen()  
+                    AppNavigation()  // ✅ CAMBIO: Usar navegación en lugar de RegistroJugadorScreen()
                 }
             }
         }
